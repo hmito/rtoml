6ecd02737c5bfa70e42d0c37891eca3e9ecaa172
Koichi Ito <hmito@outlook.com>
2023-02-19 18:53:47 +0900

    Update for experiments.

--- R/toml.R
+++ R/toml.R
@@ -1,22 +1,36 @@
-cin = file("test/sample1.toml",open="r")
+cin = file(,open="rb")
 lines = readLines(cin)
 close(cin)
 
+
+
+readBin(cin, "raw",100)
+lines <- scan("test/sample1.toml", what = character(), sep = NULL, blank.lines.skip = F)
 sep = "\n"
 str = paste0(lines, collapse=sep)
-
 as_toml_vector = function(value){
   class(value) = c(class(value),"toml_vector")
   return(value)
 }
-
+strseq = strsplit(str,"")[[1]]
+for(i in 1:length(strs)){
+  cat(strs[i])
+}
 #' TOML perser function
 #'
 #' TOML perser which read lines as much as need for create one object and return unused lines
 #' @param lines A character sequence of TOML for each line
 #' @return list; obj: generated object by the read, lines: unused lines.
 toml_perser = function(lines){
-  obj =NULL
+  mode = "#"
+
+  for(i in 1:length(strseq)){
+    c = strseq[i]
+
+    if(mode=="#"){
+      if(c==0)
+    }
+  }
   while(nrow(lines)){
     # in case Skip
     lines = lines[-1]
@@ -31,18 +45,33 @@ toml_perser = function(lines){
   return(list(obj = obj, lines=lines))
 }
 
-from_toml = function(str, sep="\n"){
-  lines_list = strsplit(str,sep,fixed=TRUE)
-  for(li in 1:length(lines_list)){
-    lines = lines_list[[li]]
+from_toml = function(str, sep=NULL){
+  if(is.null(sep)){
+    if(Sys.info()[["sysname"]]=="Windows")sep="\r\n"
+    else sep = "\n"
+  }
 
-    eval("lines")
+  space = c("\u0009","\u0020")
+  chars = strsplit(str,"")[[1]]
+  mode = ""
+  for(c in chars){
+    if(mode==""){
+
+    }else if(mode=="")
   }
 }
 
-to_toml = function(obj, sep="\n"){
+to_toml = function(obj, sep=NULL){
+  if(is.null(sep)){
+    if(Sys.info()[["sysname"]]=="Windows")sep="\r\n"
+    else sep = "\n"
+  }
 }
 
-read_toml = function(file){}
+read_toml = function(file){
+  from_toml(readChar(file, file.info(file)$size))
+}
 
-write_toml = function(obj, file){}
+write_toml = function(obj, file){
+  writeChar(to_toml(obj),file,eos=NULL)
+}
